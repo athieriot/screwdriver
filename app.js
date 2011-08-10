1df8e50cb754f365bdd255e1c0d321f5824574b2
Aurélien Thieriot <a.thieriot@gmail.com>
2011-08-10 21:02:17 +0200

    some rename

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var winston = require('winston');
-var mongo_ideas = require('./lib/mongo_ideas');
+var screwcore = require('./lib/screwcore');
 
 var app = module.exports = express.createServer();
 
@@ -29,14 +29,14 @@ app.configure('production', function(){
 });
 
 // Routes
-
+// GET
 app.get('/', function(req, res){
-   res.json('Welcome to Screwdriver server');
+   res.json(screwcore.greeting());
 });
 
 app.get('/ideas', function(req, res) {
-   mongo_ideas.all(function(err, docs) {
-      err ? res.json(err, err.status): res.json(docs, 200);
+   screwcore.ideas(function(error, ideas) {
+      error ? res.json(error, error.status) : res.json(ideas, 200);
    });
 });
 
@@ -44,19 +44,23 @@ app.get('/idea/:id([0-9]+)', function (req, res) {
    res.json('Display idea with id : ' + req.params.id);
 });
 
+// PUT
 app.put('/idea/:id([0-9]+)', function (req, res) {
    res.json('Update the idea : ' + req.params.id + ' with the data : ' + req.body);
 });
 
+// POST
 app.post('/idea', function (req, res) {
-   mongo_ideas.add(req.body.title || '', req.body.data || '', function(error) {
+   screwcore.add(req.body.title || '', req.body.data || '', function(error) {
       error && winston.info('Error on creating an idea. ' + error);
    });
 
    res.json('Idea send to mongo');
 });
 
-mongo_ideas.connect();
+// Start
+
+screwcore.connect();
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
