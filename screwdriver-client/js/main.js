58239a07da2374e117376e7af00819485749aa18
athieriot <a.thieriot@gmail.com>
2011-08-11 17:20:00 +0200

    First real interaction between HTML5 web page and node via ajax call

--- screwdriver-client/js/main.js
+++ screwdriver-client/js/main.js
@@ -3,6 +3,15 @@ require(["order!libs/jquery-1.6.2.min",
          "order!libs/modernizr-2.0.min",
          "order!libs/jquery-ui-1.8.15.custom.min"], function() {
    $(document).ready(function() {
-      $('#test').datepicker();
+      $("#idea-save").button().click(function() {
+	$.ajax({
+	   type: "POST",
+	   url: "http://localhost:3000/idea",
+	   data: {title: $("#idea-title").val(), data: $("#idea-message").val()},
+	   success: function(msg){
+	        alert( "Data Saved: " + msg );
+	      }
+	});
+      });
    });
 });
