b48b3e9c06d2ff5712c9394c47948f593054c106
athieriot <a.thieriot@gmail.com>
2011-08-11 16:23:06 +0200

    first import of a client by Html5 boilerplater

---
+++ screwdriver-site/js/main.js
@@ -0,0 +1,8 @@
+require(["order!libs/jquery-1.6.2.min",
+         "order!libs/underscore-1.1.7.min",
+         "order!libs/modernizr-2.0.min",
+         "order!libs/jquery-ui-1.8.15.custom.min"], function() {
+   $(document).ready(function() {
+      $('#test').datepicker();
+   });
+});
