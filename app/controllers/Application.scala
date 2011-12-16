d21c9161f1e3953242d35e3ed720bdbaf8996b55
Aurélien Thieriot <a.thieriot@gmail.com>
2011-12-16 23:03:26 +0100

    remove some try-fail stuff

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,20 +1,10 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
-import play.api.WS.HttpPromise
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
-  def search(value: String) = Action {
-
-    val httpPromise = WS.url("https://api.github.com/users/athieriot/gists").get
-
-    AsyncResult(httpPromise.map(r => Ok(r.getResponseBody)))
-  }
-  
 }
