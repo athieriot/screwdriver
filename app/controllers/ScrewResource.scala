a3f7f1a37e3b171484eb996191c021ea0004e779
Aurélien Thieriot <a.thieriot@gmail.com>
2011-12-17 00:38:24 +0100

    make it work with play 2.0-RC1 and add twitter bootstrap

---
+++ app/controllers/ScrewResource.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.mvc._
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aurelien
+ * Date: 16/12/11
+ * Time: 23:10
+ */
+
+object ScrewResource extends Controller {
+
+  def list = Action {NotImplemented}
+
+  def add = Action {NotImplemented}
+
+  def delete = Action {NotImplemented}
+
+}
