b846f66d950055400550d271fd73ddd101529caf
Aurélien Thieriot <a.thieriot@gmail.com>
2011-12-18 18:28:51 +0100

    choosing Salat

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      "com.mongodb.casbah" % "casbah_2.9.0-1" % "2.1.5.0"
+      "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
