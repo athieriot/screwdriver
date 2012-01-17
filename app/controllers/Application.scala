40beb6c4dd08095534f975ce9bbd09c48b36435a
Aurélien Thieriot <a.thieriot@gmail.com>
2012-01-17 01:10:32 +0100

    some header changes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,16 @@ package controllers
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index())
+  }
+
+  def search = Action {
+    Ok(views.html.search())
+  }
+
+  def about = Action {
+    Ok(views.html.about())
   }
 }
