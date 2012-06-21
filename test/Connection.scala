72a7bd205189d5db65ad199a192710e923e372c2
Aurélien Thieriot <a.thieriot@gmail.com>
2012-06-21 08:39:54 +0200

    Change the default port when testing

--- test/Connection.scala
+++ test/Connection.scala
@@ -1,20 +1,21 @@
-import com.mongodb.Mongo
-import de.flapdoodle.embedmongo.config.MongodConfig
-import de.flapdoodle.embedmongo.distribution.Version
-import de.flapdoodle.embedmongo.{MongoDBRuntime, MongodProcess, MongodExecutable}
+import de.flapdoodle.embedmongo._
+import config.MongodConfig
+import distribution.Version
 import org.specs2.specification.{AfterExample, BeforeExample}
 import play.api.test.FakeApplication
 
 trait Connection extends BeforeExample with AfterExample {
 
-  val FakeMongoApp = FakeApplication(additionalPlugins = Seq("se.radley.plugin.salat.SalatPlugin"))
+  val port: Int = 12345
+  val FakeMongoApp = FakeApplication(additionalConfiguration = Map("mongodb.default.port" -> port.toString),
+                                     additionalPlugins = Seq("se.radley.plugin.salat.SalatPlugin"))
 
   var _mongodExe: MongodExecutable = null
   var _mongod: MongodProcess = null
 
   def before() {
     val runtime: MongoDBRuntime = MongoDBRuntime. getDefaultInstance
-    _mongodExe = runtime.prepare(new MongodConfig(Version.V2_1_1, 27017, true))
+    _mongodExe = runtime.prepare(new MongodConfig(Version.V2_1_1, port, true))
     _mongod = _mongodExe.start()
   }
 
--- test/DatabaseTest.scala
+++ test/DatabaseTest.scala
@@ -1,13 +1,6 @@
-import models.Project
 import org.specs2.mutable.Specification
-
-import play.api.test._
 import play.api.test.Helpers._
-
-import com.mongodb.casbah.Imports._
-import play.api._
 import models._
-import se.radley.plugin.salat._
 
 class DatabaseTest extends Specification with Connection {
 
