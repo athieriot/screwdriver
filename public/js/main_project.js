50f20661e8538688432598b6b71acb86473d0981
Aurélien Thieriot <a.thieriot@gmail.com>
2012-07-19 21:46:27 +0200

    Step by step

--- public/js/main_project.js
+++ public/js/main_project.js
@@ -15,13 +15,46 @@ define(['lib/underscore', 'lib/ember'], function() {
             name: "My awesome and useless project"
         });
 
-            ProjectApp.ProjectDetailsView = Ember.View.create({
-            //entriesBinding: 'controller.namespace.entriesController',
-            templateName: 'project-details-template',
-            project: screwdriver
+        ProjectApp.ProjectConroller = Ember.ArrayController.create({
+            content: [screwdriver]
+        });
+
+        ProjectApp.ProjectDetailsView = Ember.CollectionView.create({
+            content: ProjectApp.ProjectConroller,
+            itemViewClass: Ember.View.extend({
+                templateName: 'project-details-template'
+            })
         });
         ProjectApp.ProjectDetailsView.appendTo('#project-details');
 
+/*
+        ProjectApp.Router = Ember.Router.extend({
+            root: Ember.Route.extend({
+
+                index: Ember.Route.extend({
+                    route: '/',
+                    connectOutlets: function( router ) {
+                        var controller = router.get( 'applicationController' );
+                        var context = controller.namespace.entriesController;
+                        context.set( 'filterBy', '' );
+
+                        // This require was left here exclusively for design purposes
+                        // Loads decoupled controller/view based on current route
+                        require([ 'app/controllers/todos', 'app/views/items' ],
+                            function( TodosController, ItemsView ) {
+                                controller.connectOutlet({
+                                    viewClass: ItemsView,
+                                    controller: TodosController.create(),
+                                    context: context
+                                });
+                            }
+                        );
+
+                    }
+                })
+            })
+        });
+*/
         ProjectApp.initialize();
         return window.ProjectApp = ProjectApp;
     });
